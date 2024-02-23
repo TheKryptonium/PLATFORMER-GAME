592f43b345f086d12004c14453d4f5296ad8cfc0
Kryptonium <lekryptonium@gmail.com>
2024-02-23 21:24:50 +0100

    Sprite loaded

--- src.py
+++ src.py
@@ -14,9 +14,44 @@ WIDTH,HEIGHT,FPS,PLAYER_VEL=1000,800,60,5
 
 window=pygame.display.set_mode((WIDTH, HEIGHT))
 
+
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path,f))]
+
+    all_sprites={}
+
+    for image in images :
+        sprite_sheet=pygame.image.load(join(path,image)).convert_alpha()
+
+        sprites=[]
+
+        for i in range (sprite_sheet.get_width() // width):
+            surface=pygame.Surface((width,height), pygame.SRCALPHA,32)
+            rect = pygame.Rect(i*width,0,width,height)
+            surface.blit(sprite_sheet,(0,0),rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction : 
+            all_sprites[image.replace(".png","")+"_right"]=sprites
+            all_sprites[image.replace(".png","")+"_left"]=flip(sprites)
+        
+        else:
+            all_sprites[image.replace("png","")]=sprites
+    
+    return all_sprites
+
+
+
+
 class Player(pygame.sprite.Sprite) : 
     COLOR=(0,0,0)
     GRAVITY = 1
+    SPRITES=load_sprite_sheets("MainCharacters","PinkMan",32,32,True)
 
     def __init__(self, x, y ,width, height):#Caractéristiques du personnage
         self.rect=pygame.Rect(x,y,width,height)
@@ -45,13 +80,14 @@ class Player(pygame.sprite.Sprite) :
             self.animation_count=0
     
     def loop(self,fps):
-        self.y_vel += min(1,(self.fall_count/fps)*self.GRAVITY)
+        # self.y_vel += min(1,(self.fall_count/fps)*self.GRAVITY)
         self.move(self.x_vel,self.y_vel)
         
         self.fall_count+=1
     
     def draw(self, win):
-        pygame.draw.rect(win,self.COLOR,self.rect)
+        self.sprite=self.SPRITES["idle_"+self.direction][0]
+        win.blit(self.sprite,(self.rect.x,self.rect.y))
 
 
 
